1ae939beda7eb6240d88ee4c40016773f5ee85a4
hckang80 <hckang80@gmail.com>
2025-02-24 11:00:15 +0900

    chore: 미사용 컴포넌트 제거

--- src/app/bible/_container.tsx
+++ src/app/bible/_container.tsx
@@ -3,13 +3,6 @@
 import type { BibleInstance, Transition } from '@/@types';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue
-} from '@/components/ui/select';
-import {
   Drawer,
   DrawerClose,
   DrawerContent,
