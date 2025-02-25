07f9cf356d3a6f85001455e4ca7d3f90e6bf045b
hckang80 <hckang80@gmail.com>
2025-02-25 15:24:48 +0900

    chore: change variable name

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export function fetcher<T>(url: string | URL | Request, init?: RequestInit): Promise<T> {
   const getUrl =
     typeof url === 'string' && !url.startsWith('http') && !url.startsWith('/api')
-      ? `${process.env.BASE_URL}${url}`
+      ? `${process.env.API_BASE_URL}${url}`
       : url;
 
   return fetch(getUrl, {
