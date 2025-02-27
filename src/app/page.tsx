bc17d2820d45ce693638220f2d660bb4e09a62ee
hckang80 <hckang80@gmail.com>
2025-02-28 01:22:48 +0900

    fix: 모바일 환경에서도 lang 두자리 고정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { headers } from 'next/headers';
 
 export default async function Home() {
   const acceptLanguage = (await headers()).get('accept-language') || 'en';
-  const [userLang] = acceptLanguage.split(',');
+  const [userLang] = acceptLanguage.split(',').slice(0, 2);
 
   redirect(`/${userLang}/bible`);
 }
