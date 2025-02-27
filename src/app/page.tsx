54c92e462116d9cababbfb301aa8a9576c5cb6c8
hckang80 <hckang80@gmail.com>
2025-02-28 01:28:40 +0900

    fix: 모바일 환경에서 언어값 두자리로 불러오기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { headers } from 'next/headers';
 
 export default async function Home() {
   const acceptLanguage = (await headers()).get('accept-language') || 'en';
-  const [userLang] = acceptLanguage.split(',').slice(0, 2);
+  const [userLang] = acceptLanguage.split(',');
 
-  redirect(`/${userLang}/bible`);
+  redirect(`/${userLang.slice(0, 2)}/bible`);
 }
