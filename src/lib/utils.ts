a65bf56cb39551a61adff9164e2aa536c6d54fe7
hckang80 <hckang80@gmail.com>
2025-03-02 02:40:40 +0900

    refactor(getLanguageFullName): 리턴 타입 고정

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,5 +21,5 @@ export function fetcher<T>(url: string | URL | Request, init?: RequestInit): Pro
 
 export function getLanguageFullName(langCode: string, locale = 'en') {
   const displayNames = new Intl.DisplayNames([locale], { type: 'language' });
-  return displayNames.of(langCode);
+  return displayNames.of(langCode) || '';
 }
