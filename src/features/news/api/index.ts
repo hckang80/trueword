471a8b3a0b5becb6681e3eb19ff4aff9ba622372
hckang80 <hckang80@gmail.com>
2025-03-14 22:00:23 +0900

    refactor(hooks): 디렉토리명 수정

---
+++ src/features/news/api/index.ts
@@ -0,0 +1,15 @@
+import { fetcher } from '@/lib/utils';
+import type { NewsItem } from '@/entities/news';
+import { useQuery } from '@tanstack/react-query';
+
+export async function fetchNews(): Promise<NewsItem[]> {
+  return await fetcher<NewsItem[]>('/api/news');
+}
+
+export function useNews() {
+  return useQuery<NewsItem[]>({
+    queryKey: ['news'],
+    queryFn: fetchNews,
+    staleTime: 1000 * 60 * 15
+  });
+}
