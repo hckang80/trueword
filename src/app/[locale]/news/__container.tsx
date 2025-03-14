7247458f403b42bd9533761d90c736f97a05cbca
hckang80 <hckang80@gmail.com>
2025-03-14 14:04:56 +0900

    feat: page 파일은 서버사이드 전용으로 분리

---
+++ src/app/[locale]/news/__container.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useNews } from '@/app/features/news';
+
+export default function NewsContainer() {
+  const { data: news, isLoading, isError } = useNews();
+
+  if (isLoading) return <div className="text-center py-10">뉴스를 불러오는 중입니다...</div>;
+  if (isError)
+    return <div className="text-center py-10 text-red-500">뉴스를 불러오는 데 실패했습니다.</div>;
+
+  return <pre className="whitespace-pre-wrap p-[20px]">{JSON.stringify(news, null, 2)}</pre>;
+}
--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -1,13 +1,5 @@
-'use client';
+import Container from '../news/__container';
 
-import { useNews } from '@/app/features/news/hooks';
-
-export default function News() {
-  const { data: news, isLoading, isError } = useNews();
-
-  if (isLoading) return <div className="text-center py-10">뉴스를 불러오는 중입니다...</div>;
-  if (isError)
-    return <div className="text-center py-10 text-red-500">뉴스를 불러오는 데 실패했습니다.</div>;
-
-  return <pre className="whitespace-pre-wrap p-[20px]">{JSON.stringify(news, null, 2)}</pre>;
+export default function NewsPage() {
+  return <Container />;
 }
