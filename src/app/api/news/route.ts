759a0e1075ac4078bea5eae3947debe161bf3e34
hckang80 <hckang80@gmail.com>
2025-03-15 17:11:25 +0900

    chore: 국문 RSS로 변경

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -3,7 +3,16 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const rssFeeds = [{ url: 'https://www.christianity.com/rss', name: 'Christianity Today' }];
+    const rssFeeds = [
+      {
+        url: 'https://www.christiantoday.co.kr/rss',
+        name: '크리스천투데이'
+      },
+      {
+        url: 'https://www.christiandaily.co.kr/rss',
+        name: '기독일보'
+      }
+    ];
 
     const newsPromises = rssFeeds.map((source) => fetchRssFeed(source.url, source.name));
 
