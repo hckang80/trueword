8eea03168d0ab4a855fef7a50d24c8fe7e64f130
hckang80 <hckang80@gmail.com>
2025-03-24 12:07:31 +0900

    refactor(locale): 공용 함수 종류별로 구분

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,11 +1,2 @@
 export * from './axiosInstance';
-
-export const isSupportedLocale = (locale: string): locale is 'en' | 'ko' => {
-  return ['en', 'ko'].includes(locale);
-};
-
-export const locales = ['en', 'ko'] as const;
-
-export type Locale = (typeof locales)[number];
-
-export const DEFAULT_LOCALE: Locale = 'ko';
+export * from './locale';
