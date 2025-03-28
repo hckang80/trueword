d69187a26841c6c3b871d5cf76df3308b90e152a
hckang80 <hckang80@gmail.com>
2025-03-28 10:01:30 +0900

    feat(news): 헤더 영역 스타일링

--- src/app/[locale]/news/[...source]/__container.tsx
+++ src/app/[locale]/news/[...source]/__container.tsx
@@ -1,23 +1,25 @@
 'use client';
 
 import type { NewsItem } from '@/entities/news';
+import { toReadableDate } from '@/shared';
 import Link from 'next/link';
 import ReactMarkdown from 'react-markdown';
 
 export default function NewsIdContainer({
   summary,
-  news: { link }
+  news: { link, title, source, pubDate }
 }: {
   summary: string;
   news: NewsItem;
 }) {
   return (
     <article className="p-[var(--global-inset)]">
-      <header className="mb-[10px]">
-        <h2 className="text-lg font-medium">요약 결과</h2>
-        <p>이 글은 AI가 원문을 분석하여 핵심 내용을 요약한 것입니다.</p>
-      </header>
-      <div className="text-gray-700 whitespace-pre-line bg-gray-100 p-[var(--global-inset)]">
+      <NewsHeader title={title} source={source} pubDate={pubDate} />
+
+      <div className="text-gray-700 bg-gray-100 p-[var(--global-inset)]">
+        <p className="mb-[10px] text-xs text-muted-foreground">
+          이 글은 AI가 원문을 분석하여 핵심 내용을 요약한 것입니다.
+        </p>
         <ReactMarkdown>{summary}</ReactMarkdown>
       </div>
 
@@ -29,3 +31,24 @@ export default function NewsIdContainer({
     </article>
   );
 }
+
+const NewsHeader = ({
+  title,
+  source,
+  pubDate
+}: {
+  title: string;
+  source: string;
+  pubDate: string;
+}) => (
+  <header className="mb-[10px]">
+    <h1 className="text-lg font-medium">{title}</h1>
+    <div className="flex items-center text-xs md:text-sm text-gray-500 dark:text-gray-400">
+      <span>{source}</span>
+      <span className="mx-1.5 sm:mx-2">•</span>
+      <span>{toReadableDate(new Date(pubDate))}</span>
+    </div>
+  </header>
+);
+
+NewsHeader.displayName = 'NewsHeader';
