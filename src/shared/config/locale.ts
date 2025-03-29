e113ae4df772788c476db56b887918b6065686b5
hckang80 <hckang80@gmail.com>
2025-03-30 00:47:23 +0900

    feat(supportedTranslations): locales에 맞춰 번역본 지원하는 함수 생성

--- src/shared/config/locale.ts
+++ src/shared/config/locale.ts
@@ -7,3 +7,10 @@ export const locales = ['en', 'ko'] as const;
 export type Locale = (typeof locales)[number];
 
 export const DEFAULT_LOCALE: Locale = 'ko';
+
+export const supportedTranslations = locales.map((locale) => getLocaleDisplayName(locale));
+
+function getLocaleDisplayName(locale: Locale, targetLocale: string = 'en') {
+  const displayNames = new Intl.DisplayNames([targetLocale], { type: 'language' });
+  return displayNames.of(locale) || locale;
+}
