957cae8a4608705019e94902c534a3358acd0d28
hckang80 <hckang80@gmail.com>
2025-03-31 10:17:23 +0900

    chore: 중문 번역본 추가

--- src/shared/config/locale.ts
+++ src/shared/config/locale.ts
@@ -1,8 +1,8 @@
-export const isSupportedLocale = (locale: string): locale is 'en' | 'ko' => {
-  return ['en', 'ko'].includes(locale);
+export const isSupportedLocale = (locale: string): locale is 'en' | 'ko' | 'zh' => {
+  return ['en', 'ko', 'zh'].includes(locale);
 };
 
-export const locales = ['en', 'ko'] as const;
+export const locales = ['en', 'ko', 'zh'] as const;
 
 export type Locale = (typeof locales)[number];
 
