c091e194d0206117ddaadc57220c714f018a66a2
hckang80 <hckang80@gmail.com>
2025-04-25 23:25:33 +0900

    style: 로고 굵기 수정

--- src/app/[locale]/Header.tsx
+++ src/app/[locale]/Header.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from './ModeToggle';
 function Header() {
   return (
     <header className="flex justify-between pl-[var(--global-inset)] pr-[var(--global-inset)] pt-[10px] pb-[10px] text-right">
-      <h1 className="uppercase text-3xl shimmer-text">True Word</h1>
+      <h1 className="uppercase text-3xl font-bold shimmer-text">True Word</h1>
       <ModeToggle />
     </header>
   );
