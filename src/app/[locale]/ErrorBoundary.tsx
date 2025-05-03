ef1a7c17d2e19821c5dccc76944e5481a3f94473
hckang80 <hckang80@gmail.com>
2025-05-03 23:30:07 +0900

    feat: 에러 요소 추가

--- src/app/[locale]/ErrorBoundary.tsx
+++ src/app/[locale]/ErrorBoundary.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Button } from '@/shared/components/ui/button';
 import React from 'react';
 
 interface Props {
@@ -8,39 +9,39 @@ interface Props {
 
 interface State {
   hasError: boolean;
+  error: Error | null;
+  errorInfo: React.ErrorInfo | null;
 }
 
 class ErrorBoundary extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    // 오류가 있는지 여부를 추적하는 상태 변수 정의
-    this.state = { hasError: false };
+    this.state = { hasError: false, error: null, errorInfo: null };
   }
   static getDerivedStateFromError(error: Error) {
-    // 다음 렌더링에서 폴백 UI를 표시하도록 상태 업데이트
-
-    return { hasError: true };
+    return { hasError: true, error };
   }
-  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    // 여기에서 자체 오류 로깅 서비스를 사용할 수 있습니다
-    console.log({ error, errorInfo });
+  componentDidCatch(_error: Error, errorInfo: React.ErrorInfo) {
+    this.setState({ errorInfo });
   }
   render() {
-    // 오류가 발생했는지 확인
     if (this.state.hasError) {
-      // 사용자 정의 폴백 UI를 렌더링할 수 있습니다
       return (
         <div>
-          <h2>Oops, there is an error!</h2>
-          <button type="button" onClick={() => this.setState({ hasError: false })}>
+          <h2>{this.state.error?.toString()}</h2>
+          {this.state.errorInfo && (
+            <details style={{ whiteSpace: 'pre-wrap' }}>
+              <summary>에러상세</summary>
+              {this.state.errorInfo.componentStack}
+            </details>
+          )}
+          <Button type="button" onClick={() => this.setState({ hasError: false })}>
             Try again?
-          </button>
+          </Button>
         </div>
       );
     }
-
-    // 오류가 없는 경우 자식 컴포넌트를 반환
 
     return this.props.children;
   }
