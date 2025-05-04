7354ca9c0b7632254be9691c044741a5a54b4f64
hckang80 <hckang80@gmail.com>
2025-05-04 22:41:54 +0900

    feat: 이메일 문의 버튼 추가

--- src/app/[locale]/Header.tsx
+++ src/app/[locale]/Header.tsx
@@ -1,9 +1,17 @@
+import { Button } from '@/shared/components/ui/button';
 import { ModeToggle } from './ModeToggle';
+import { Mail } from 'lucide-react';
+import Link from 'next/link';
 
 function Header() {
   return (
     <header className="flex justify-between pl-[var(--global-inset)] pr-[var(--global-inset)] pt-[10px] pb-[10px] text-right">
       <h1 className="uppercase text-3xl font-bold shimmer-text">True Word</h1>
+      <Button variant="outline" size="icon" asChild>
+        <a href="mailto:hckang80@gmail.com">
+          <Mail />
+        </a>
+      </Button>
       <ModeToggle />
     </header>
   );
