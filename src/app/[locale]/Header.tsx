38c0e8d356797bea571bc4e53117bfd5f9a46684
hckang80 <hckang80@gmail.com>
2025-05-04 22:46:26 +0900

    style(Header): 영역 스타일링

--- src/app/[locale]/Header.tsx
+++ src/app/[locale]/Header.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link';
 
 function Header() {
   return (
-    <header className="flex justify-between pl-[var(--global-inset)] pr-[var(--global-inset)] pt-[10px] pb-[10px] text-right">
-      <h1 className="uppercase text-3xl font-bold shimmer-text">True Word</h1>
+    <header className="flex justify-end gap-[4px] pl-[var(--global-inset)] pr-[var(--global-inset)] pt-[10px] pb-[10px] text-right">
+      <h1 className="mr-auto uppercase text-3xl font-bold shimmer-text">True Word</h1>
       <Button variant="outline" size="icon" asChild>
         <a href="mailto:hckang80@gmail.com">
           <Mail />
