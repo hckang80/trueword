85faf87a60dbcf00763639b3d05d07a6d66eb903
hckang80 <hckang80@gmail.com>
2025-05-07 15:13:31 +0900

    feat: UI 프로그래스바 교체

--- src/app/[locale]/ProgressBar.tsx
+++ src/app/[locale]/ProgressBar.tsx
@@ -1,24 +1,5 @@
-'use client';
+import NextTopLoader from 'nextjs-toploader';
 
-import { useEffect } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
-
-NProgress.configure({ showSpinner: false });
-
-export default function ProgressBar() {
-  if (!NProgress.isStarted()) {
-    NProgress.start();
-  }
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const timer = setTimeout(() => NProgress.done(), 500);
-
-    return () => clearTimeout(timer);
-  }, [pathname, searchParams]);
-
-  return null;
+export default function ProgressBar({ ...props }) {
+  return <NextTopLoader {...props} />;
 }
