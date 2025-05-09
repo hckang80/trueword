67be5035b6186d6f6ad28462ef4418a58bfe855b
hckang80 <hckang80@gmail.com>
2025-05-09 21:50:41 +0900

    test: add suspense

--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -2,6 +2,8 @@ import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query
 import Container from '../news/__container';
 import { newsQueryOptions } from '@/features/news';
 import type { Metadata, ResolvingMetadata } from 'next';
+import { Suspense } from 'react';
+import { LoaderCircle } from 'lucide-react';
 
 export async function generateMetadata(
   _props: unknown,
@@ -24,8 +26,16 @@ export default async function NewsPage() {
   const dehydratedState = dehydrate(queryClient);
 
   return (
-    <HydrationBoundary state={dehydratedState}>
-      <Container />
-    </HydrationBoundary>
+    <Suspense
+      fallback={
+        <div className="center-absolute">
+          <LoaderCircle className="animate-spin" />
+        </div>
+      }
+    >
+      <HydrationBoundary state={dehydratedState}>
+        <Container />
+      </HydrationBoundary>
+    </Suspense>
   );
 }
