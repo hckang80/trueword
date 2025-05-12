2eeb969fb5100d1884803aa867dfd1755d8b3d32
hckang80 <hckang80@gmail.com>
2025-05-12 11:11:46 +0900

    test: 서버 응답 시간 콘솔 추가

--- src/app/[locale]/news/[...source]/page.tsx
+++ src/app/[locale]/news/[...source]/page.tsx
@@ -13,13 +13,19 @@ export default async function NewsIdPage({ params }: Props) {
   const { source: sources } = await params;
 
   const queryClient = new QueryClient();
+  console.time('useNewsBySource');
   const news = await queryClient.fetchQuery(newsBySourceQueryOptions(sources));
   const newsBySource = getNewsItem(news, sources);
+  console.timeEnd('useNewsBySource');
   if (!newsBySource) return <p>찾으시는 뉴스 결과가 없습니다.</p>;
 
+  console.time('useScrapedContent');
   const scraped = await queryClient.fetchQuery(scrapedContentQueryOptions(newsBySource.link));
+  console.timeEnd('useScrapedContent');
 
+  console.time('useSummary');
   await queryClient.prefetchQuery(summaryQueryOptions(scraped.content, scraped.title));
+  console.timeEnd('useSummary');
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
