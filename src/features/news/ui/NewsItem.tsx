5e14774169fc524f259f87798e295e170e12a6b2
hckang80 <hckang80@gmail.com>
2025-05-15 23:16:43 +0900

    refactor(ViewTransition): 중복 코드 개선

--- src/features/news/ui/NewsItem.tsx
+++ src/features/news/ui/NewsItem.tsx
@@ -13,21 +13,19 @@ const NewsItem = ({ item }: { item: NewsItemType }) => (
       href={`${usePathname()}/${item.sourceEng}/${item.guid}`}
       className="group flex items-center justify-between gap-[8px] visited:text-purple-600 p-[20px] border border-gray-200 rounded-lg mb-4"
     >
-      <ViewTransition name={`title-${item.sourceEng}-${item.guid}`}>
+      <ViewTransition name={`news-header-${item.sourceEng}-${item.guid}`}>
         <div>
           <h2 className="text-sm sm:text-base md:text-lg font-semibold mb-2 visited:not:text-gray-900 dark:text-white group-hover:text-blue-600 transition-colors duration-300">
             <strong>{item.title}</strong>
           </h2>
           <NewsItemMeta source={item.source} pubDate={item.pubDate} />
         </div>
-      </ViewTransition>
-      {item.thumbnail && (
-        <ViewTransition name={`thumbnail-${item.sourceEng}-${item.guid}`}>
+        {item.thumbnail && (
           <div className="relative w-[120px] h-[63px] shrink-0 rounded-lg overflow-hidden relative bg-primary/10">
             <NewsImage src={item.thumbnail} />
           </div>
-        </ViewTransition>
-      )}
+        )}
+      </ViewTransition>
     </Link>
   </li>
 );
