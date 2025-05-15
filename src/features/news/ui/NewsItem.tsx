7b7d0a0f32adce267e05fb1dafa7454baa74d388
hckang80 <hckang80@gmail.com>
2025-05-15 22:19:46 +0900

    fix: Link 컴포넌트 임포트 오류 수정

--- src/features/news/ui/NewsItem.tsx
+++ src/features/news/ui/NewsItem.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Link } from 'lucide-react';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { memo } from 'react';
 import { NewsImage, NewsItemMeta } from '.';
