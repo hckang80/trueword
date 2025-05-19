f3b8e9fb3756a2006b60252a92fea61eebdc9c60
hckang80 <hckang80@gmail.com>
2025-05-19 12:05:38 +0900

    fix: alt 명시적 선언. 경고 제거

--- src/features/news/ui/NewsImage.tsx
+++ src/features/news/ui/NewsImage.tsx
@@ -3,8 +3,8 @@
 import Image, { ImageProps } from 'next/image';
 import { memo } from 'react';
 
-const NewsImage = ({ ...props }: ImageProps) => (
-  <Image priority fill {...props} style={{ objectFit: 'cover' }} />
+const NewsImage = ({ alt = '', ...props }: ImageProps) => (
+  <Image priority fill alt={alt} {...props} style={{ objectFit: 'cover' }} />
 );
 NewsImage.displayName = 'NewsImage';
 
