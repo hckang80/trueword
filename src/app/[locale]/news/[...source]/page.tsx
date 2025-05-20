ad74740a2ac782b85c8c93883914376e9c6735f0
hckang80 <hckang80@gmail.com>
2025-05-20 11:40:13 +0900

    refactor: 뉴스 상세 관련 데이터를 클라이언트 패치

--- src/app/[locale]/news/[...source]/page.tsx
+++ src/app/[locale]/news/[...source]/page.tsx
@@ -1,13 +1,7 @@
-import {
-  getNewsItem,
-  newsQueryOptions,
-  scrapedContentQueryOptions,
-  summaryQueryOptions
-} from '@/features/news';
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { getNewsItem, newsQueryOptions } from '@/features/news';
+import { QueryClient } from '@tanstack/react-query';
 import Container from './__container';
 import type { Metadata, ResolvingMetadata } from 'next';
-import { getTranslations } from 'next-intl/server';
 
 type Props = { params: Promise<{ locale: string; source: string[] }> };
 
@@ -29,26 +23,6 @@ export async function generateMetadata(
   };
 }
 
-export default async function NewsIdPage({ params }: Props) {
-  const { locale, source: sources } = await params;
-  const t = await getTranslations('News');
-
-  const queryClient = new QueryClient();
-
-  const news = await queryClient.fetchQuery(newsQueryOptions(locale));
-  const newsBySource = getNewsItem(news, sources);
-
-  if (!newsBySource) return <p className="center-absolute">{t('noNews')}</p>;
-
-  const scraped = await queryClient.fetchQuery(
-    scrapedContentQueryOptions(newsBySource.link, newsBySource.description)
-  );
-
-  await queryClient.prefetchQuery(summaryQueryOptions(scraped.content, scraped.title, locale));
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Container />
-    </HydrationBoundary>
-  );
+export default async function NewsIdPage() {
+  return <Container />;
 }
