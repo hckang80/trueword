dd317db03cb933dbdde33b27457bc303cc7ebf8a
hckang80 <hckang80@gmail.com>
2025-05-22 07:00:22 +0900

    style: 마크업 구조 개선

--- src/features/bible/ui/VideoList.tsx
+++ src/features/bible/ui/VideoList.tsx
@@ -38,9 +38,9 @@ function VideoList({ chapterName }: { chapterName: string }) {
         <DrawerHeader>
           <DrawerTitle>{chapterName}</DrawerTitle>
         </DrawerHeader>
-        {isLoading ? (
-          <div className="grid gap-4 py-4 px-4">
-            {Array.from({ length: VIDEO_LENGTH }).map((_, i) => (
+        <div className="grid gap-4 py-4 px-4">
+          {isLoading ? (
+            Array.from({ length: VIDEO_LENGTH }).map((_, i) => (
               <div key={i} className="flex gap-4 items-start">
                 <div className="flex-shrink-0 relative">
                   <Skeleton className="w-[128px] h-[72px] rounded-md" />
@@ -50,50 +50,54 @@ function VideoList({ chapterName }: { chapterName: string }) {
                   <Skeleton className="w-30 h-4" />
                 </div>
               </div>
-            ))}
-          </div>
-        ) : (
-          <div className="grid gap-4 py-4 px-4">
-            {selectedVideoId ? (
-              <div className="mb-4">
-                <div className="relative pb-[56.25%] h-0 overflow-hidden rounded-lg">
-                  <iframe
-                    src={`https://www.youtube.com/embed/${selectedVideoId}?autoplay=1`}
-                    title="YouTube video player"
-                    className="absolute top-0 left-0 w-full h-full"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                  ></iframe>
-                </div>
-                <Button variant="outline" className="mt-2" onClick={() => setSelectedVideoId(null)}>
-                  {t('BackToList')}
-                </Button>
-              </div>
-            ) : (
-              videos.map((video) => (
-                <div
-                  key={video.id}
-                  className="flex gap-4 items-start cursor-pointer"
-                  onClick={() => handleVideoClick(video.id)}
-                >
-                  <div className="flex-shrink-0 relative">
-                    <Image
-                      src={video.thumbnail}
-                      width="128"
-                      height="72"
-                      alt={video.title}
-                      className="rounded-md"
-                    />
+            ))
+          ) : (
+            <>
+              {selectedVideoId ? (
+                <div className="mb-4">
+                  <div className="relative pb-[56.25%] h-0 overflow-hidden rounded-lg">
+                    <iframe
+                      src={`https://www.youtube.com/embed/${selectedVideoId}?autoplay=1`}
+                      title="YouTube video player"
+                      className="absolute top-0 left-0 w-full h-full"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    ></iframe>
                   </div>
-                  <div className="flex flex-col">
-                    <span className="font-medium hover:underline">{video.title}</span>
-                    <span className="text-sm text-gray-500">{video.channelTitle}</span>
-                  </div>
+                  <Button
+                    variant="outline"
+                    className="mt-2"
+                    onClick={() => setSelectedVideoId(null)}
+                  >
+                    {t('BackToList')}
+                  </Button>
                 </div>
-              ))
-            )}
-          </div>
-        )}
+              ) : (
+                videos.map((video) => (
+                  <div
+                    key={video.id}
+                    className="flex gap-4 items-start cursor-pointer"
+                    onClick={() => handleVideoClick(video.id)}
+                  >
+                    <div className="flex-shrink-0 relative">
+                      <Image
+                        src={video.thumbnail}
+                        width="128"
+                        height="72"
+                        alt={video.title}
+                        className="rounded-md"
+                      />
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="font-medium hover:underline">{video.title}</span>
+                      <span className="text-sm text-gray-500">{video.channelTitle}</span>
+                    </div>
+                  </div>
+                ))
+              )}
+            </>
+          )}
+        </div>
       </DrawerContent>
     </Drawer>
   );
