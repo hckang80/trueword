9d5fb5b522a5583c0d8b52893f67bce247cd85ea
hckang80 <hckang80@gmail.com>
2025-05-21 13:57:36 +0900

    refactor(components): ui로 폴더 변경

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,6 +1,1 @@
-export * from './BottomNavigation';
-export * from './Header';
-export * from './InfiniteScrollTrigger';
-export * from './Loading';
-export * from './ModeToggle';
-export * from './ProgressBar';
+export * from './ui';
---
+++ src/shared/components/ui/index.ts
@@ -0,0 +1,6 @@
+export * from './BottomNavigation';
+export * from './Header';
+export * from './InfiniteScrollTrigger';
+export * from './Loading';
+export * from './ModeToggle';
+export * from './ProgressBar';
