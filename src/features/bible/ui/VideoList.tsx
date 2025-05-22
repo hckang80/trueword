88f602a7deb6bc3805fcc077f7633588dd4c44f1
hckang80 <hckang80@gmail.com>
2025-05-22 19:40:04 +0900

    style: 마크업 & 스타일링

--- src/features/bible/ui/VideoList.tsx
+++ src/features/bible/ui/VideoList.tsx
@@ -7,6 +7,7 @@ import {
   DrawerContent,
   DrawerHeader,
   DrawerTitle,
+  DrawerDescription,
   DrawerTrigger,
   Skeleton
 } from '@/shared';
@@ -31,73 +32,79 @@ function VideoList({ chapterName }: { chapterName: string }) {
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
         <Button variant="outline" size="icon">
-          <Video className="h-4 w-4" />
+          <Video />
         </Button>
       </DrawerTrigger>
-      <DrawerContent className="sm:max-w-[600px]" aria-describedby="관련 영상 목록">
+      <DrawerContent>
         <DrawerHeader>
-          <DrawerTitle>{chapterName}</DrawerTitle>
-        </DrawerHeader>
-        <div className="grid gap-4 py-4 px-4">
-          {isLoading ? (
-            Array.from({ length: VIDEO_LENGTH }).map((_, i) => (
-              <div key={i} className="flex gap-4 items-start">
-                <div className="flex-shrink-0 relative">
-                  <Skeleton className="w-[128px] h-[72px] rounded-md" />
-                </div>
-                <div className="flex flex-col grow-1 gap-1">
-                  <Skeleton className="w-full h-6" />
-                  <Skeleton className="w-30 h-4" />
-                </div>
-              </div>
-            ))
-          ) : (
-            <>
-              {selectedVideoId ? (
-                <div className="mb-4">
-                  <div className="relative pb-[56.25%] h-0 overflow-hidden rounded-lg">
-                    <iframe
-                      src={`https://www.youtube.com/embed/${selectedVideoId}?autoplay=1`}
-                      title="YouTube video player"
-                      className="absolute top-0 left-0 w-full h-full"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      allowFullScreen
-                    ></iframe>
-                  </div>
-                  <Button
-                    variant="outline"
-                    className="mt-2"
-                    onClick={() => setSelectedVideoId(null)}
-                  >
-                    {t('BackToList')}
-                  </Button>
-                </div>
-              ) : (
-                videos.map((video) => (
-                  <div
-                    key={video.id}
-                    className="flex gap-4 items-start cursor-pointer"
-                    onClick={() => handleVideoClick(video.id)}
-                  >
-                    <div className="flex-shrink-0 relative">
-                      <Image
-                        src={video.thumbnail}
-                        width="128"
-                        height="72"
-                        alt={video.title}
-                        className="rounded-md"
-                      />
+          <DrawerTitle hidden>{chapterName}</DrawerTitle>
+          <DrawerDescription asChild>
+            <div className="grid gap-[10px]">
+              {isLoading ? (
+                Array.from({ length: VIDEO_LENGTH }).map((_, i) => (
+                  <div key={i} className="flex gap-4">
+                    <div className="shrink-0 relative">
+                      <Skeleton className="w-[128px] h-[72px] rounded-md" />
                     </div>
-                    <div className="flex flex-col">
-                      <span className="font-medium hover:underline">{video.title}</span>
-                      <span className="text-sm text-gray-500">{video.channelTitle}</span>
+                    <div className="flex flex-col grow-1 gap-1">
+                      <Skeleton className="w-full h-6" />
+                      <Skeleton className="w-30 h-4" />
                     </div>
                   </div>
                 ))
+              ) : (
+                <>
+                  {selectedVideoId ? (
+                    <div className="mb-4">
+                      <div className="relative pb-[56.25%] h-0 overflow-hidden rounded-lg">
+                        <iframe
+                          src={`https://www.youtube.com/embed/${selectedVideoId}?autoplay=1`}
+                          title="YouTube video player"
+                          className="absolute top-0 left-0 w-full h-full"
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                          allowFullScreen
+                        ></iframe>
+                      </div>
+                      <Button
+                        variant="outline"
+                        className="mt-2"
+                        onClick={() => setSelectedVideoId(null)}
+                      >
+                        {t('BackToList')}
+                      </Button>
+                    </div>
+                  ) : (
+                    videos.map((video) => (
+                      <button
+                        key={video.id}
+                        className="flex gap-4"
+                        onClick={() => handleVideoClick(video.id)}
+                      >
+                        <div className="shrink-0 relative">
+                          <Image
+                            src={video.thumbnail}
+                            width="128"
+                            height="72"
+                            alt={video.title}
+                            className="rounded-md"
+                          />
+                        </div>
+                        <div className="flex flex-col grow-1 gap-1 text-left">
+                          <span className="text-sm text-gray-900 dark:text-white hover:underline">
+                            {video.title}
+                          </span>
+                          <span className="text-xs text-gray-500 dark:text-gray-400">
+                            {video.channelTitle}
+                          </span>
+                        </div>
+                      </button>
+                    ))
+                  )}
+                </>
               )}
-            </>
-          )}
-        </div>
+            </div>
+          </DrawerDescription>
+        </DrawerHeader>
       </DrawerContent>
     </Drawer>
   );
