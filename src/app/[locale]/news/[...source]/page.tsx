9a60312aeb4e67c4fde8c967864dedc24e98e2d8
hckang80 <hckang80@gmail.com>
2025-05-28 00:03:59 +0900

    fix(meta): 무한 루프 오류로 임시 제거

--- src/app/[locale]/news/[...source]/page.tsx
+++ src/app/[locale]/news/[...source]/page.tsx
@@ -5,23 +5,23 @@ import type { Metadata, ResolvingMetadata } from 'next';
 
 type Props = { params: Promise<{ locale: string; source: string[] }> };
 
-export async function generateMetadata(
-  { params }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const previousTitle = (await parent).title;
+// export async function generateMetadata(
+//   { params }: Props,
+//   parent: ResolvingMetadata
+// ): Promise<Metadata> {
+//   const previousTitle = (await parent).title;
 
-  const { locale, source: sources } = await params;
+//   const { locale, source: sources } = await params;
 
-  const queryClient = new QueryClient();
+//   const queryClient = new QueryClient();
 
-  const news = await queryClient.fetchQuery(newsQueryOptions(locale));
-  const newsBySource = getNewsItem(news, sources);
+//   const news = await queryClient.fetchQuery(newsQueryOptions(locale));
+//   const newsBySource = getNewsItem(news, sources);
 
-  return {
-    title: `${newsBySource?.title} - ${previousTitle?.absolute}`
-  };
-}
+//   return {
+//     title: `${newsBySource?.title} - ${previousTitle?.absolute}`
+//   };
+// } 무한 루프 임시 제거
 
 export default async function NewsIdPage() {
   return <Container />;
