f9b636fcea5fd45a9268273f835b9e507d17600e
hckang80 <hckang80@gmail.com>
2025-06-03 21:11:18 +0900

    revert: 메타 타이틀 복원

--- src/app/[locale]/news/[...source]/page.tsx
+++ src/app/[locale]/news/[...source]/page.tsx
@@ -1,36 +1,25 @@
-// import { getNewsItem, newsQueryOptions } from '@/features/news';
-// import { QueryClient } from '@tanstack/react-query';
+import { getNewsItem, newsQueryOptions } from '@/features/news';
+import { QueryClient } from '@tanstack/react-query';
 import Container from './__container';
 import type { Metadata, ResolvingMetadata } from 'next';
 
-// type Props = { params: Promise<{ locale: string; source: string[] }> };
-
-// export async function generateMetadata(
-//   { params }: Props,
-//   parent: ResolvingMetadata
-// ): Promise<Metadata> {
-//   const previousTitle = (await parent).title;
-
-//   const { locale, source: sources } = await params;
-
-//   const queryClient = new QueryClient();
-
-//   const news = await queryClient.fetchQuery(newsQueryOptions(locale));
-//   const newsBySource = getNewsItem(news, sources);
-
-//   return {
-//     title: `${newsBySource?.title} - ${previousTitle?.absolute}`
-//   };
-// }
+type Props = { params: Promise<{ locale: string; source: string[] }> };
 
 export async function generateMetadata(
-  _props: unknown,
+  { params }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
   const previousTitle = (await parent).title;
 
+  const { locale, source: sources } = await params;
+
+  const queryClient = new QueryClient();
+
+  const news = await queryClient.fetchQuery(newsQueryOptions(locale));
+  const newsBySource = getNewsItem(news, sources);
+
   return {
-    title: `News - ${previousTitle?.absolute}`
+    title: `${newsBySource?.title} - ${previousTitle?.absolute}`
   };
 }
 
