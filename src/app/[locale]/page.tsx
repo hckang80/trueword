34011795fe35e86e6f80cd5b056ceeef56c7faaa
hckang80 <hckang80@gmail.com>
2025-06-06 23:48:31 +0900

    feat: 오늘의 말씀 prefetchQuery 추가

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,11 @@
 import {
   bibleChapterInstanceQueryOptions,
+  bibleTodayQueryOptions,
   CHAPTER_LENGTH,
   localizedTranslationVersionsQueryOptions
 } from '@/features/bible';
-import { getRandomPositiveInt } from '@/shared';
-import { QueryClient } from '@tanstack/react-query';
+import { axiosInstance, getRandomPositiveInt } from '@/shared';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import React from 'react';
 
 type Props = {
@@ -18,19 +19,20 @@ const MainPage = async ({ params }: Props) => {
   const { locale: language } = await params;
 
   const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(bibleTodayQueryOptions);
+  const dehydratedState = dehydrate(queryClient);
 
   const localizedTranslationVersions = await queryClient.fetchQuery(
     localizedTranslationVersionsQueryOptions(language)
   );
 
+  const { data: todayWord } = await axiosInstance.get('/api/bible/today');
   const [defaultTranslation] = localizedTranslationVersions;
   const getTranslationVersionId = defaultTranslation.abbreviation;
 
   const data = await queryClient.fetchQuery(
     bibleChapterInstanceQueryOptions([getTranslationVersionId, '' + bookNumber, '' + chapterNumber])
   );
-  const todayWord = data.verses[getRandomPositiveInt(data.verses.length) - 1];
-  console.log({ todayWord });
 
   const mainStyle = {
     backgroundColor: '#222',
@@ -73,64 +75,66 @@ const MainPage = async ({ params }: Props) => {
   };
 
   return (
-    <div style={mainStyle}>
-      <div style={sectionStyle}>
-        <h2 style={headingStyle}>오늘의 말씀</h2>
-        <p style={{ fontSize: '1.2em', color: '#FFF' }}>{todayWord.name}</p>
-        <p>{todayWord.text}</p>
-        <a href="#" style={linkStyle}>
-          전체 맥락 보기
-        </a>
-      </div>
-
-      <div style={sectionStyle}>
-        <h2 style={headingStyle}>최신 뉴스</h2>
-        <ul>
-          <li>
-            <a href="#" style={newsLinkStyle}>
-              속보: [뉴스 제목 1]
-            </a>
-          </li>
-          <li>
-            <a href="#" style={newsLinkStyle}>
-              주요: [뉴스 제목 2]
-            </a>
-          </li>
-          <li>
-            <a href="#" style={newsLinkStyle}>
-              [뉴스 제목 3]
-            </a>
-          </li>
-        </ul>
-        <a href="#" style={linkStyle}>
-          더 많은 뉴스 보기
-        </a>
-      </div>
-
-      <div>
-        <h2 style={headingStyle}>빠른 링크</h2>
-        <p>
-          <a href="#" style={(newsLinkStyle, { marginRight: '10px' })}>
-            성경 검색
+    <HydrationBoundary state={dehydratedState}>
+      <div style={mainStyle}>
+        <div style={sectionStyle}>
+          <h2 style={headingStyle}>오늘의 말씀</h2>
+          <p style={{ fontSize: '1.2em', color: '#FFF' }}>{todayWord.name}</p>
+          <p>{todayWord.text}</p>
+          <a href="#" style={linkStyle}>
+            전체 맥락 보기
           </a>
-          <a href="#" style={(newsLinkStyle, { marginRight: '10px' })}>
-            인물 이야기
-          </a>
-        </p>
-      </div>
+        </div>
 
-      <div style={fixedNavStyle}>
-        <a href="#" style={navLinkStyle}>
-          설정
-        </a>
-        <a href="#" style={navLinkStyle}>
-          뉴스
-        </a>
-        <a href="#" style={navLinkStyle}>
-          성경
-        </a>
+        <div style={sectionStyle}>
+          <h2 style={headingStyle}>최신 뉴스</h2>
+          <ul>
+            <li>
+              <a href="#" style={newsLinkStyle}>
+                속보: [뉴스 제목 1]
+              </a>
+            </li>
+            <li>
+              <a href="#" style={newsLinkStyle}>
+                주요: [뉴스 제목 2]
+              </a>
+            </li>
+            <li>
+              <a href="#" style={newsLinkStyle}>
+                [뉴스 제목 3]
+              </a>
+            </li>
+          </ul>
+          <a href="#" style={linkStyle}>
+            더 많은 뉴스 보기
+          </a>
+        </div>
+
+        <div>
+          <h2 style={headingStyle}>빠른 링크</h2>
+          <p>
+            <a href="#" style={(newsLinkStyle, { marginRight: '10px' })}>
+              성경 검색
+            </a>
+            <a href="#" style={(newsLinkStyle, { marginRight: '10px' })}>
+              인물 이야기
+            </a>
+          </p>
+        </div>
+
+        <div style={fixedNavStyle}>
+          <a href="#" style={navLinkStyle}>
+            설정
+          </a>
+          <a href="#" style={navLinkStyle}>
+            뉴스
+          </a>
+          <a href="#" style={navLinkStyle}>
+            성경
+          </a>
+        </div>
       </div>
-    </div>
+    </HydrationBoundary>
   );
 };
 
