d41332425975c1943050ccc1ba5fcde2f0f48d59
hckang80 <hckang80@gmail.com>
2025-06-07 00:57:39 +0900

    feat: 해당 성경 절로 포커싱 처리

--- src/features/bible/ui/VerseList.tsx
+++ src/features/bible/ui/VerseList.tsx
@@ -1,12 +1,31 @@
 'use client';
 
+import { useRef, useEffect } from 'react';
+import { useBibleSearchParams } from '../hooks';
 import type { Verse } from '../model';
 
 function VerseList({ selectedVerses }: { selectedVerses: Verse[] }) {
+  const { verseNumber } = useBibleSearchParams();
+  const verseRefs = useRef<Record<number, HTMLParagraphElement | null>>({});
+
+  useEffect(() => {
+    const verseEl = verseNumber && verseRefs.current[+verseNumber];
+    if (!verseEl) return;
+    verseEl.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center'
+    });
+  }, [verseNumber]);
+
   return (
     <div>
       {selectedVerses.map(({ verse, text }) => (
-        <p key={verse}>
+        <p
+          key={verse}
+          ref={(el) => {
+            verseRefs.current[verse] = el;
+          }}
+        >
           <sup>{verse}</sup> {text}
         </p>
       ))}
