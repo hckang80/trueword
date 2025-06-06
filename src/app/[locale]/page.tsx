2f3e6a5fce12be98981ee5e817a160fe3a486639
hckang80 <hckang80@gmail.com>
2025-06-06 11:04:56 +0900

    feat: 오늘의 말씀 출력하기

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -76,11 +76,8 @@ const MainPage = async ({ params }: Props) => {
     <div style={mainStyle}>
       <div style={sectionStyle}>
         <h2 style={headingStyle}>오늘의 말씀</h2>
-        <p style={{ fontSize: '1.2em', color: '#FFF' }}>요한복음 3:16</p>
-        <p>
-          하나님이 세상을 이처럼 사랑하사 독생자를 주셨으니 이는 그를 믿는 자마다 멸망하지 않고
-          영생을 얻게 하려 하심이라.
-        </p>
+        <p style={{ fontSize: '1.2em', color: '#FFF' }}>{todayWord.name}</p>
+        <p>{todayWord.text}</p>
         <a href="#" style={linkStyle}>
           전체 맥락 보기
         </a>
