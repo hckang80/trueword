55ae556d9e91aa9166abd26136f4dff3fd3d66f1
hckang80 <hckang80@gmail.com>
2025-06-06 20:56:20 +0900

    feat: 오늘의 말씀 redis 캐싱

--- src/app/api/bible/today/route.ts
+++ src/app/api/bible/today/route.ts
@@ -1,10 +1,26 @@
 import { CHAPTER_LENGTH, fetchBibleInstance, fetchTranslationVersions } from '@/features/bible';
 import { DEFAULT_LOCALE, getRandomPositiveInt } from '@/shared';
+import { Redis } from '@upstash/redis';
 import { NextRequest, NextResponse } from 'next/server';
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN
+});
+
+const CACHE_TTL = 24 * 60 * 60;
+
+const utcDateKey = new Date().toISOString().split('T')[0];
 
 export async function GET(request: NextRequest) {
   try {
     const locale = request.headers.get('Accept-Language') || DEFAULT_LOCALE;
+
+    const key = `todayVerse:${locale}:${utcDateKey}`;
+    const cached = await redis.get<string>(key);
+
+    if (cached) return NextResponse.json(cached);
+
     const translationVersions = await fetchTranslationVersions();
     const localizedTranslationVersion = translationVersions.find(({ lang }) => lang === locale);
 
@@ -19,6 +35,8 @@ export async function GET(request: NextRequest) {
       '' + chapterNumber
     ]);
     const randomVerse = verses[getRandomPositiveInt(verses.length) - 1];
+
+    redis.set(key, randomVerse, { ex: CACHE_TTL });
 
     return NextResponse.json(randomVerse);
   } catch (error) {
