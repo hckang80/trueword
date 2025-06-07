e7861a3470667661b509264950719413a52fff72
hckang80 <hckang80@gmail.com>
2025-06-07 11:16:05 +0900

    refactor: 불필요한 요소 제거

--- src/app/[locale]/__container.tsx
+++ src/app/[locale]/__container.tsx
@@ -8,45 +8,6 @@ import type { UrlObject } from 'url';
 
 export default function MainContainer() {
   const { locale } = useParams<{ locale: string }>();
-  const mainStyle = {
-    backgroundColor: '#222',
-    color: '#EEE',
-    padding: '20px',
-    fontFamily: 'sans-serif' // 기본 폰트 설정
-  };
-
-  const sectionStyle = {
-    marginBottom: '20px',
-    paddingBottom: '15px',
-    borderBottom: '1px solid #555'
-  };
-
-  const headingStyle = {
-    color: '#FFF',
-    marginTop: '0'
-  };
-
-  const linkStyle = {
-    color: '#89CFF0',
-    textDecoration: 'none'
-  };
-
-  const newsLinkStyle = {
-    color: '#EEE',
-    textDecoration: 'none'
-  };
-
-  const fixedNavStyle = {
-    position: 'fixed',
-    bottom: '20px',
-    right: '20px'
-  };
-
-  const navLinkStyle = {
-    color: '#EEE',
-    textDecoration: 'none',
-    marginLeft: '10px'
-  };
 
   const {
     data: { verse, ...restData }
@@ -64,62 +25,12 @@ export default function MainContainer() {
   const t = useTranslations('Home');
 
   return (
-    <div style={mainStyle}>
-      <div style={sectionStyle}>
-        <h2 style={headingStyle}>{t('todaysVerse')}</h2>
-        <p style={{ fontSize: '1.2em', color: '#FFF' }}>{verse.name}</p>
-        <p>{verse.text}</p>
-        <Link href={moreTodayWord} style={linkStyle}>
-          {t('viewFullContext')}
-        </Link>
-      </div>
-
-      <div style={sectionStyle}>
-        <h2 style={headingStyle}>최신 뉴스</h2>
-        <ul>
-          <li>
-            <a href="#" style={newsLinkStyle}>
-              속보: [뉴스 제목 1]
-            </a>
-          </li>
-          <li>
-            <a href="#" style={newsLinkStyle}>
-              주요: [뉴스 제목 2]
-            </a>
-          </li>
-          <li>
-            <a href="#" style={newsLinkStyle}>
-              [뉴스 제목 3]
-            </a>
-          </li>
-        </ul>
-        <a href="#" style={linkStyle}>
-          더 많은 뉴스 보기
-        </a>
-      </div>
-
+    <div>
       <div>
-        <h2 style={headingStyle}>빠른 링크</h2>
-        <p>
-          <a href="#" style={(newsLinkStyle, { marginRight: '10px' })}>
-            성경 검색
-          </a>
-          <a href="#" style={(newsLinkStyle, { marginRight: '10px' })}>
-            인물 이야기
-          </a>
-        </p>
-      </div>
-
-      <div style={fixedNavStyle}>
-        <a href="#" style={navLinkStyle}>
-          설정
-        </a>
-        <a href="#" style={navLinkStyle}>
-          뉴스
-        </a>
-        <a href="#" style={navLinkStyle}>
-          성경
-        </a>
+        <h2>{t('todaysVerse')}</h2>
+        <p>{verse.name}</p>
+        <p>{verse.text}</p>
+        <Link href={moreTodayWord}>{t('viewFullContext')}</Link>
       </div>
     </div>
   );
