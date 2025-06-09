72173427adb276fbc703cd45fc3d6e46e1a6e6c3
hckang80 <hckang80@gmail.com>
2025-06-09 23:56:39 +0900

    style: 국제화 버튼 추가

--- src/shared/components/ui/Header.tsx
+++ src/shared/components/ui/Header.tsx
@@ -1,10 +1,18 @@
+'use client';
+
+import { useBibleLanguage } from '@/features/bible';
 import { Button, ModeToggle } from '@/shared';
 import { Mail } from 'lucide-react';
+import Link from 'next/link';
 
 export function Header() {
+  const language = useBibleLanguage();
   return (
     <header className="guide-line-layout flex justify-end gap-[4px] pl-[var(--global-inset)] pr-[var(--global-inset)] pt-[10px] pb-[10px] text-right">
       <h1 className="mr-auto uppercase text-3xl font-bold shimmer-text">True Word</h1>
+      <Button variant="ghost" size="icon" asChild>
+        <Link href="#">{language}</Link>
+      </Button>
       <Button variant="outline" size="icon" asChild>
         <a href="mailto:hckang80@gmail.com">
           <Mail />
