f720a5312879867e7d91292d323601cdcf08d725
hckang80 <hckang80@gmail.com>
2025-06-10 00:05:15 +0900

    feat(language): 셀렉트 요소로 변경

--- src/shared/components/ui/Header.tsx
+++ src/shared/components/ui/Header.tsx
@@ -1,15 +1,41 @@
 'use client';
 
 import { useBibleLanguage } from '@/features/bible';
-import { Button, ModeToggle } from '@/shared';
+import {
+  Button,
+  ModeToggle,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/shared';
 import { Mail } from 'lucide-react';
 import Link from 'next/link';
+
+const languages = ['ko', 'en'];
+
+const handleChange = (language: string) => {
+  console.log(language);
+};
 
 export function Header() {
   const language = useBibleLanguage();
   return (
     <header className="guide-line-layout flex justify-end gap-[4px] pl-[var(--global-inset)] pr-[var(--global-inset)] pt-[10px] pb-[10px] text-right">
       <h1 className="mr-auto uppercase text-3xl font-bold shimmer-text">True Word</h1>
+      <Select value={language} onValueChange={handleChange}>
+        <SelectTrigger className="w-[80px]">
+          <SelectValue placeholder="Select a language" />
+        </SelectTrigger>
+        <SelectContent>
+          {languages.map((language) => (
+            <SelectItem value={language} key={language}>
+              {language.toUpperCase()}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
       <Button variant="ghost" size="icon" asChild>
         <Link href="#">{language}</Link>
       </Button>
