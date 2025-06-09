79a9f10a38ff88b35b3b4c50ec4ab14900e6d830
hckang80 <hckang80@gmail.com>
2025-06-10 00:12:14 +0900

    feat: 언어 변경 기능 추가

--- src/shared/components/ui/Header.tsx
+++ src/shared/components/ui/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useBibleLanguage } from '@/features/bible';
+import { useBibleParams } from '@/features/bible';
 import {
   Button,
   ModeToggle,
@@ -10,21 +10,26 @@ import {
   SelectTrigger,
   SelectValue
 } from '@/shared';
+import { usePathname } from '@/shared/i18n/routing';
 import { Mail } from 'lucide-react';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const languages = ['ko', 'en'];
 
-const handleChange = (language: string) => {
-  console.log(language);
-};
-
 export function Header() {
-  const language = useBibleLanguage();
+  const router = useRouter();
+  const { locale } = useBibleParams();
+  const pathname = usePathname();
+
+  const handleChange = (language: string) => {
+    router.push(`/${language}${pathname}`);
+  };
+
   return (
     <header className="guide-line-layout flex justify-end gap-[4px] pl-[var(--global-inset)] pr-[var(--global-inset)] pt-[10px] pb-[10px] text-right">
       <h1 className="mr-auto uppercase text-3xl font-bold shimmer-text">True Word</h1>
-      <Select value={language} onValueChange={handleChange}>
+      <Select value={locale} onValueChange={handleChange}>
         <SelectTrigger className="w-[80px]">
           <SelectValue placeholder="Select a language" />
         </SelectTrigger>
@@ -37,7 +42,7 @@ export function Header() {
         </SelectContent>
       </Select>
       <Button variant="ghost" size="icon" asChild>
-        <Link href="#">{language}</Link>
+        <Link href="#">{locale}</Link>
       </Button>
       <Button variant="outline" size="icon" asChild>
         <a href="mailto:hckang80@gmail.com">
