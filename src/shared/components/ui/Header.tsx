135af73411dcaed71c8770d411fbcd07a8cbdb38
hckang80 <hckang80@gmail.com>
2025-06-11 13:08:19 +0900

    style: 로고에 홈 링크 연결

--- src/shared/components/ui/Header.tsx
+++ src/shared/components/ui/Header.tsx
@@ -11,7 +11,7 @@ import {
   SelectTrigger,
   SelectValue
 } from '@/shared';
-import { usePathname } from '@/shared/i18n/routing';
+import { Link, usePathname } from '@/shared/i18n/routing';
 import { Mail } from 'lucide-react';
 import { useRouter } from 'nextjs-toploader/app';
 
@@ -26,7 +26,9 @@ export function Header() {
 
   return (
     <header className="guide-line-layout flex justify-end gap-[4px] pl-[var(--global-inset)] pr-[var(--global-inset)] pt-[10px] pb-[10px] text-right">
-      <h1 className="mr-auto uppercase text-3xl font-bold shimmer-text">True Word</h1>
+      <h1 className="mr-auto uppercase text-3xl font-bold shimmer-text">
+        <Link href="/home">True Word</Link>
+      </h1>
       <Select value={locale} onValueChange={handleChange}>
         <SelectTrigger className="w-[80px]">
           <SelectValue placeholder="Select a language" />
