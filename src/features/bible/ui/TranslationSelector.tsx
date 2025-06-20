b853055bc4844e112ca9dc56c36ac02016c631c1
hckang80 <hckang80@gmail.com>
2025-06-20 23:37:44 +0900

    refactor: 불필요요소 제거

--- src/features/bible/ui/TranslationSelector.tsx
+++ src/features/bible/ui/TranslationSelector.tsx
@@ -10,21 +10,13 @@ import {
   DrawerTitle,
   DrawerDescription
 } from '@/shared';
-import { ChevronDown, Globe } from 'lucide-react';
+import { ChevronDown } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 import { useSearchParams } from 'next/navigation';
 import { useState, useEffect, useRef } from 'react';
 import { useTranslationVersions, useUpdateBibleParams } from '../hooks';
-import type { BibleTransition, BibleChapterInstance } from '../model';
-import { BibleLanguages } from '..';
 
-function TranslationSelector({
-  localizedTranslationVersions,
-  bibleChapterInstance
-}: {
-  localizedTranslationVersions: BibleTransition[];
-  bibleChapterInstance: BibleChapterInstance;
-}) {
+function TranslationSelector() {
   const t = useTranslations('Common');
   const [open, setOpen] = useState(false);
 
@@ -37,9 +29,6 @@ function TranslationSelector({
   const searchParams = useSearchParams();
   const abbreviation = searchParams.get('abbreviation');
   const translationLanguage = searchParams.get('translation');
-  const { short_name: label } =
-    localizedTranslationVersions.find((version) => version.short_name === abbreviation) ||
-    localizedTranslationVersions[0];
 
   const { data: translationVersions } = useTranslationVersions();
 
@@ -83,42 +72,13 @@ function TranslationSelector({
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
-        <Button variant="outline">{label}</Button>
+        <Button variant="outline">{abbreviation}</Button>
       </DrawerTrigger>
       <DrawerContent>
         <DrawerHeader className="p-0">
-          <div className="flex items-center justify-between p-[10px]">
-            <span className="flex items-center gap-[4px] capitalize">
-              <Globe />
-              {t('language')}
-            </span>
-            <div className="flex items-center gap-[4px]">
-              <BibleLanguages />
-            </div>
-          </div>
           <DrawerTitle className="hidden">Translations</DrawerTitle>
           <DrawerDescription asChild>
             <div>
-              <ul>
-                {localizedTranslationVersions.map(({ full_name, short_name }) => (
-                  <li key={short_name}>
-                    <button
-                      className={cn('w-full p-[10px] text-left')}
-                      onClick={() => handleTranslationVersionChange(short_name)}
-                    >
-                      <em
-                        className={cn(
-                          'block text-[16px]',
-                          short_name === bibleChapterInstance.abbreviation ? 'font-bold' : ''
-                        )}
-                      >
-                        {short_name}
-                      </em>
-                      <span className="block text-[13px]">{full_name}</span>
-                    </button>
-                  </li>
-                ))}
-              </ul>
               {translationVersions.map(({ id, language, translations }, index) => (
                 <details
                   name="translationVersions"
