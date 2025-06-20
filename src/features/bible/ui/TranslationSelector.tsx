a3fa2e95011cc75a6acce49427da880d26397e53
hckang80 <hckang80@gmail.com>
2025-06-20 23:21:11 +0900

    feat: 번역본 선택 UI 변경

--- src/features/bible/ui/TranslationSelector.tsx
+++ src/features/bible/ui/TranslationSelector.tsx
@@ -10,11 +10,11 @@ import {
   DrawerTitle,
   DrawerDescription
 } from '@/shared';
-import { Globe } from 'lucide-react';
+import { ChevronDown, Globe } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 import { useSearchParams } from 'next/navigation';
-import { useState, useEffect } from 'react';
-import { useUpdateBibleParams } from '../hooks';
+import { useState, useEffect, useRef } from 'react';
+import { useTranslationVersions, useUpdateBibleParams } from '../hooks';
 import type { BibleTransition, BibleChapterInstance } from '../model';
 import { BibleLanguages } from '..';
 
@@ -30,15 +30,51 @@ function TranslationSelector({
 
   const updateBibleParams = useUpdateBibleParams();
 
-  const handleTranslationVersionChange = (abbreviation: string) => {
-    updateBibleParams({ abbreviation });
+  const handleTranslationVersionChange = (abbreviation: string, id: string) => {
+    updateBibleParams({ abbreviation, language: id });
   };
 
   const searchParams = useSearchParams();
   const abbreviation = searchParams.get('abbreviation');
+  const translationLanguage = searchParams.get('translation');
   const { short_name: label } =
     localizedTranslationVersions.find((version) => version.short_name === abbreviation) ||
     localizedTranslationVersions[0];
+
+  const { data: translationVersions } = useTranslationVersions();
+
+  const detailsRefs = useRef<Record<number, HTMLDetailsElement | null>>({});
+  const timeoutRefs = useRef<Record<number, NodeJS.Timeout | null>>({});
+
+  const selectedTranslationVersionItem = (short_name: string) => short_name === abbreviation;
+
+  const adjustPosition = (index: number) => {
+    const details = detailsRefs.current[index];
+    if (!details || !details.open) return;
+
+    const viewportHeight = window.innerHeight;
+    let rect: DOMRect | null = null;
+    let timeout = timeoutRefs.current[index];
+
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(() => {
+      rect = details.getBoundingClientRect();
+
+      const isOutside = rect.bottom > viewportHeight;
+
+      if (isOutside) {
+        details.scrollIntoView({
+          behavior: 'smooth',
+          block: 'nearest'
+        });
+      }
+
+      timeout = null;
+    }, 100);
+  };
 
   useEffect(() => {
     setOpen(false);
@@ -62,26 +98,63 @@ function TranslationSelector({
           </div>
           <DrawerTitle className="hidden">Translations</DrawerTitle>
           <DrawerDescription asChild>
-            <ul>
-              {localizedTranslationVersions.map(({ full_name, short_name }) => (
-                <li key={short_name}>
-                  <button
-                    className={cn('w-full p-[10px] text-left')}
-                    onClick={() => handleTranslationVersionChange(short_name)}
-                  >
-                    <em
-                      className={cn(
-                        'block text-[16px]',
-                        short_name === bibleChapterInstance.abbreviation ? 'font-bold' : ''
-                      )}
+            <div>
+              <ul>
+                {localizedTranslationVersions.map(({ full_name, short_name }) => (
+                  <li key={short_name}>
+                    <button
+                      className={cn('w-full p-[10px] text-left')}
+                      onClick={() => handleTranslationVersionChange(short_name)}
                     >
-                      {short_name}
-                    </em>
-                    <span className="block text-[13px]">{full_name}</span>
-                  </button>
-                </li>
+                      <em
+                        className={cn(
+                          'block text-[16px]',
+                          short_name === bibleChapterInstance.abbreviation ? 'font-bold' : ''
+                        )}
+                      >
+                        {short_name}
+                      </em>
+                      <span className="block text-[13px]">{full_name}</span>
+                    </button>
+                  </li>
+                ))}
+              </ul>
+              {translationVersions.map(({ id, language, translations }, index) => (
+                <details
+                  name="translationVersions"
+                  ref={(el) => {
+                    detailsRefs.current[index] = el;
+                  }}
+                  key={id}
+                  className="group transition-[max-height] duration-400 ease-in-out max-h-[80px] open:max-h-[8000px]"
+                  onToggle={() => adjustPosition(index)}
+                >
+                  <summary
+                    className={cn(
+                      'flex justify-between p-[10px]',
+                      id === translationLanguage ? 'font-bold' : ''
+                    )}
+                  >
+                    {language}
+                    <ChevronDown size={20} className="transition group-open:rotate-180" />
+                  </summary>
+                  <div className="grid grid-cols-5 gap-[4px] px-[10px]">
+                    {translations.map(({ short_name }) => (
+                      <Button
+                        key={short_name}
+                        variant="outline"
+                        disabled={selectedTranslationVersionItem(short_name)}
+                        onClick={() => {
+                          handleTranslationVersionChange(short_name, id);
+                        }}
+                      >
+                        {short_name}
+                      </Button>
+                    ))}
+                  </div>
+                </details>
               ))}
-            </ul>
+            </div>
           </DrawerDescription>
         </DrawerHeader>
       </DrawerContent>
