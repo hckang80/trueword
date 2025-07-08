ea7b2f7ce23adcc82749742dd5381610cbd2df43
hckang80 <hckang80@gmail.com>
2025-07-09 00:37:31 +0900

    chore: 함수 주석 추가

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -26,6 +26,15 @@ export function extractUniqId(url: string) {
   return match?.[1] || '';
 }
 
+/**
+ * 1부터 max(포함)까지의 랜덤한 양의 정수를 반환합니다.
+ *
+ * @param {number} max - 반환할 수 있는 최대 정수값(1 이상이어야 함)
+ * @returns {number} 1 이상 max 이하의 랜덤 정수
+ *
+ * @example
+ * getRandomPositiveInt(5); // 1, 2, 3, 4, 5 중 하나 반환
+ */
 export function getRandomPositiveInt(max: number) {
   return Math.floor(Math.random() * max) + 1;
 }
