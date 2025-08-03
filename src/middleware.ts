120a9a86eaad7a34fd71af5635c04f5a4dd435f5
hckang80 <hckang80@gmail.com>
2025-08-03 17:29:29 +0900

    chore(supabase): 미들웨어 설정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,16 @@
-import createMiddleware from 'next-intl/middleware';
 import { routing } from '@/shared/i18n/routing';
+import { updateSession } from '@/shared/lib/supabase/middleware';
+import createMiddleware from 'next-intl/middleware';
+import type { NextRequest } from 'next/server';
 
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: [`/(en|ko)/:path*`] // TODO: en|ko -> locales를 이용해 동적삽입 필요
+  matcher: [
+    `/(en|ko)/:path*`, // TODO: en|ko -> locales를 이용해 동적삽입 필요
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'
+  ]
 };
