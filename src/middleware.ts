86506ba0a6d2364ab24691e5f9cb026ced6fad0f
hckang80 <hckang80@gmail.com>
2025-08-06 00:42:46 +0900

    fix: 성경 페이지 API 요청 오류 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,5 +22,7 @@ export default async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)']
+  matcher: [
+    '/((?!translations|api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'
+  ]
 };
