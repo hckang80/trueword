321f22ad89734a3d4ce3f7e7f82ad4da09a3adf1
hckang80 <hckang80@gmail.com>
2025-08-05 09:14:50 +0900

    fix: 진입시 로그인 페이지가는 이슈 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,23 @@
 import { routing } from '@/shared/i18n/routing';
 import createMiddleware from 'next-intl/middleware';
-import type { NextRequest } from 'next/server';
-import { locales } from './shared';
-import { updateSession } from './shared/lib/supabase/middleware';
 
-const privatePages = [''];
+// const privatePages = [''];
 
-const handleI18nRouting = createMiddleware(routing);
+// const handleI18nRouting = createMiddleware(routing);
 
-export default function middleware(request: NextRequest) {
-  const privatePathnameRegex = RegExp(
-    `^(/(${locales.join('|')}))?(${privatePages
-      .flatMap((p) => (p === '/' ? ['', '/'] : p))
-      .join('|')})/?$`,
-    'i'
-  );
-  const isPrivatePage = privatePathnameRegex.test(request.nextUrl.pathname);
+// export default function middleware(request: NextRequest) {
+//   const privatePathnameRegex = RegExp(
+//     `^(/(${locales.join('|')}))?(${privatePages
+//       .flatMap((p) => (p === '/' ? ['', '/'] : p))
+//       .join('|')})/?$`,
+//     'i'
+//   );
+//   const isPrivatePage = privatePathnameRegex.test(request.nextUrl.pathname);
 
-  return isPrivatePage ? updateSession(request) : handleI18nRouting(request);
-}
+//   return isPrivatePage ? updateSession(request) : handleI18nRouting(request);
+// }
+export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/', '/(en|ko)/:path*']
+  matcher: ['/(en|ko)/:path*']
 };
