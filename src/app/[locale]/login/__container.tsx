71078ad6e8a5be7b830451a671c8317ede1cd55d
hckang80 <hckang80@gmail.com>
2025-08-08 21:45:17 +0900

    style: 텍스트 애니메이션 추가

--- src/app/[locale]/login/__container.tsx
+++ src/app/[locale]/login/__container.tsx
@@ -2,10 +2,11 @@
 
 import { Button, GoogleIcon } from '@/shared';
 import { createClient } from '@/shared/lib/supabase/client';
-import { Flex, Grid } from '@radix-ui/themes';
+import { Flex } from '@radix-ui/themes';
 import { User } from '@supabase/supabase-js';
 import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
+import { TextAnimation } from './TextAnimation';
 
 export default function LoginContainer() {
   const [user, setUser] = useState<User | null>(null);
@@ -54,25 +55,29 @@ export default function LoginContainer() {
   };
 
   return (
-    <div className="center-absolute">
-      <Grid gap="2">
-        <span className="sr-only">{user?.user_metadata.full_name}</span>
-        <button
-          onClick={handleGoogleLogin}
-          className="w-full flex items-center justify-center gap-3 px-6 py-3 dark:bg-white bg-black dark:text-black text-white rounded-sm shadow-sm font-semibold"
-        >
-          <GoogleIcon />
-          Sign in with Google
-        </button>
+    <Flex
+      direction="column"
+      justify="center"
+      gap="2"
+      className="absolute guide-line-layout inset-y-0"
+    >
+      <TextAnimation />
+      <span className="sr-only">{user?.user_metadata.full_name}</span>
+      <button
+        onClick={handleGoogleLogin}
+        className="w-full flex items-center justify-center gap-3 px-6 py-3 dark:bg-white bg-black dark:text-black text-white rounded-sm shadow-sm font-semibold"
+      >
+        <GoogleIcon />
+        Sign in with Google
+      </button>
 
-        {!!user && (
-          <Flex justify="center">
-            <Button variant="link" onClick={handleLogout}>
-              LOGOUT
-            </Button>
-          </Flex>
-        )}
-      </Grid>
-    </div>
+      {!!user && (
+        <Flex justify="center">
+          <Button variant="link" onClick={handleLogout}>
+            LOGOUT
+          </Button>
+        </Flex>
+      )}
+    </Flex>
   );
 }
