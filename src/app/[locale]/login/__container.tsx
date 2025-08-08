5f60ad91fb62807314a2ba9e0694cdc715c9c252
hckang80 <hckang80@gmail.com>
2025-08-08 21:13:45 +0900

    refactor: 불필요한 요소 제거

--- src/app/[locale]/login/__container.tsx
+++ src/app/[locale]/login/__container.tsx
@@ -6,20 +6,12 @@ import { Flex, Grid } from '@radix-ui/themes';
 import { User } from '@supabase/supabase-js';
 import { useEffect, useState } from 'react';
 
-export default function LoginContainer({
-  nonce,
-  hashedNonce
-}: {
-  nonce: string;
-  hashedNonce: string;
-}) {
+export default function LoginContainer() {
   const [user, setUser] = useState<User | null>(null);
   const supabase = createClient();
 
-  // Google OAuth 로그인 핸들러
   const handleGoogleLogin = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
-      // provider: 'google',
       provider: 'google',
       options: {
         redirectTo: `${window.location.origin}/api/auth/callback`
@@ -55,7 +47,6 @@ export default function LoginContainer({
     const { error } = await supabase.auth.signOut();
     if (error) {
       console.error('Logout error:', error);
-      console.log('로그아웃에 실패했습니다.');
     } else {
       console.log('로그아웃 되었습니다.');
     }
@@ -63,7 +54,7 @@ export default function LoginContainer({
 
   return (
     <Grid gap="2">
-      {user?.user_metadata.full_name}
+      <span className="sr-only">{user?.user_metadata.full_name}</span>
       <button
         onClick={handleGoogleLogin}
         className="w-full flex items-center justify-center gap-3 px-6 py-3 dark:bg-white bg-black dark:text-black text-white rounded-sm shadow-sm font-semibold"
--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,22 +1,19 @@
-import { generateNonce } from '@/shared';
 import LoginContainer from './__container';
-import { login, signup } from './actions';
 
 export default async function LoginPage() {
-  const [nonce, hashedNonce] = await generateNonce();
-
   return (
-    <>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email" required />
-        <label htmlFor="password">Password:</label>
-        <input id="password" name="password" type="password" required />
-        <button formAction={login}>Log in</button>
-        <button formAction={signup}>Sign up</button>
-      </form>
+    // <>
+    //   <form>
+    //     <label htmlFor="email">Email:</label>
+    //     <input id="email" name="email" type="email" required />
+    //     <label htmlFor="password">Password:</label>
+    //     <input id="password" name="password" type="password" required />
+    //     <button formAction={login}>Log in</button>
+    //     <button formAction={signup}>Sign up</button>
+    //   </form>
 
-      <LoginContainer nonce={nonce} hashedNonce={hashedNonce} />
-    </>
+    //   <LoginContainer />
+    // </>
+    <LoginContainer />
   );
 }
