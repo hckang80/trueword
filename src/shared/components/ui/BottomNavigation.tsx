0b3109f05cfc3b7f8b65b65b4c7384db652913eb
hckang80 <hckang80@gmail.com>
2025-08-09 16:28:45 +0900

    test

--- src/shared/components/ui/BottomNavigation.tsx
+++ src/shared/components/ui/BottomNavigation.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { translationVersionCode } from '@/features/bible';
-import { Link, usePathname } from '@/shared/i18n/routing';
 import { Book, Home, Newspaper } from 'lucide-react';
+import { Link, usePathname } from '@/shared/i18n/routing';
 import { useLocale, useTranslations } from 'next-intl';
+import { translationVersionCode } from '@/features/bible';
 
 interface NavItemProps {
   href: string;
