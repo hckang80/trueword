8562dba40e68048db8a93258f0c964840a539bd2
hckang80 <hckang80@gmail.com>
2025-08-12 22:29:58 +0900

    feat: 뉴스 탭시 스토리지에 저장

--- src/features/news/ui/NewsItem.tsx
+++ src/features/news/ui/NewsItem.tsx
@@ -1,38 +1,43 @@
 'use client';
 
 import { Card, CardContent } from '@/shared/components';
+import { useLocalStorage } from '@uidotdev/usehooks';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { memo, unstable_ViewTransition as ViewTransition } from 'react';
 import { NewsImage, NewsItemMeta } from '.';
 import type { NewsItemType } from '../model';
 
-const NewsItem = ({ item }: { item: NewsItemType }) => (
-  <li>
-    <Card>
-      <CardContent>
-        <Link
-          href={`${usePathname()}/${item.sourceEng}/${item.guid}`}
-          className="group flex items-center justify-between gap-2 visited:text-gray-300 dark:visited:text-gray-600"
-        >
-          <ViewTransition name={`news-header-${item.sourceEng}-${item.guid}`}>
-            <div>
-              <h2 className="font-semibold mb-2 visited:not:text-gray-900 dark:visited:not:text-white group-hover:underline">
-                <strong>{item.title}</strong>
-              </h2>
-              <NewsItemMeta source={item.source} pubDate={item.pubDate} />
-            </div>
-            {item.thumbnail && (
-              <div className="relative aspect-video basis-[120px] shrink-0 rounded-lg overflow-hidden bg-primary/10">
-                <NewsImage src={item.thumbnail} alt="" />
+const NewsItem = ({ item }: { item: NewsItemType }) => {
+  const [_news, saveNews] = useLocalStorage<string[]>('visitedNews', []);
+  return (
+    <li>
+      <Card>
+        <CardContent>
+          <Link
+            href={`${usePathname()}/${item.sourceEng}/${item.guid}`}
+            onClick={() => saveNews((prev) => [...prev, `${item.sourceEng}/${item.guid}`])}
+            className="group flex items-center justify-between gap-2 visited:text-gray-300 dark:visited:text-gray-600"
+          >
+            <ViewTransition name={`news-header-${item.sourceEng}-${item.guid}`}>
+              <div>
+                <h2 className="font-semibold mb-2 visited:not:text-gray-900 dark:visited:not:text-white group-hover:underline">
+                  <strong>{item.title}</strong>
+                </h2>
+                <NewsItemMeta source={item.source} pubDate={item.pubDate} />
               </div>
-            )}
-          </ViewTransition>
-        </Link>
-      </CardContent>
-    </Card>
-  </li>
-);
+              {item.thumbnail && (
+                <div className="relative aspect-video basis-[120px] shrink-0 rounded-lg overflow-hidden bg-primary/10">
+                  <NewsImage src={item.thumbnail} alt="" />
+                </div>
+              )}
+            </ViewTransition>
+          </Link>
+        </CardContent>
+      </Card>
+    </li>
+  );
+};
 NewsItem.displayName = 'NewsItem';
 
 export default memo(NewsItem);
