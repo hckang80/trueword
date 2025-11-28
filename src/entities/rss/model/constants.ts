ee3f4f660e323ee265c553f5129cd17d68338249
hckang80 <hckang80@gmail.com>
2025-11-28 09:43:22 +0900

    chore(news): RSS 업데이트

--- src/entities/rss/model/constants.ts
+++ src/entities/rss/model/constants.ts
@@ -2,14 +2,14 @@ import { RSSFeed } from './types';
 
 // config 설정과도 연관이 있으므로 업데이트 후 편집기를 재실행해야 반영됩니다.
 export const RSS_FEEDS: RSSFeed[] = [
-  // {
-  //   url: 'https://relevantmagazine.com/feed',
-  //   imageUrl: 'i0.wp.com',
-  //   name: {
-  //     global: 'relevantmagazine'
-  //   },
-  //   locale: 'en'
-  // },
+  {
+    url: 'https://relevantmagazine.com/feed',
+    imageUrl: 'i0.wp.com',
+    name: {
+      global: 'relevantmagazine'
+    },
+    locale: 'en'
+  },
   {
     url: 'https://www.christianitytoday.com/rss',
     name: {
